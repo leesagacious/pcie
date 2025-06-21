72c036c08199dacc2e0370ee50085721fe6da43d
Lizhe <sensor1010@163.com>
2025-06-21 11:10:29 -0700

    PCI_COMMAND register
    Signed-off-by: Lizhe <sensor1010@163.com>

--- pci.c
+++ pci.c
@@ -53,7 +53,20 @@ static int do_pci_enable_device(struct pci_dev *dev, int bars)
 	 * via message-based interrupts)
 	 */
 	pci_read_config_byte(dev, PCI_INTERRUPT_PIN, &pin);
+	/*
+	 * only handle INTx configuration if the device has declared a valid interrupt pin
+	 * condition1 : the device is not using MSI/MSI-X(guaranteed by the outer if statement)
+	 * condition2 : the device actually supports legacy interrupts(pin != 0)
+	 */
 	if (pin) {
+		/*
+		 * read the PCI_COMMAND register(offset 0x04)
+		 * this register controls basic device functions
+		 * bit 0: I/O space enable
+		 * bit 1: memory space enable
+		 * bit 2: bus master enable
+		 * bit 3: INTx interrupt disable control
+		 */
 		pci_read_config_word(dev, PCI_COMMAND, &cmd);
 		if (cmd & PCI_COMMAND_INTX_DISABLE)
 			pci_write_config_word(dev, PCI_COMMAND,
