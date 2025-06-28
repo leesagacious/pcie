efc61026a38405953923e472448c10bacb7e0e48
Lizhe <sensor1010@163.com>
2025-06-28 08:12:22 -0700

    host: get axi and remap
    Signed-off-by: Lizhe <sensor1010@163.com>

--- pcie-rockchip.c
+++ pcie-rockchip.c
@@ -3,6 +3,13 @@ int rockchip_pcie_parse_dt(struct rockchip_pcie *rockchip)
 {
 	int err;
 
+	regs = platform_get_resource_byname(pdev,
+				IORESOURCE_MEM,
+				"axi-base");
+	rockchip->reg_base = devm_pci_remap_cfg_resource(dev, regs);
+	if (IS_ERR(rockchip->reg_base))
+		return PTR_ERR(rockchip->reg_base);
+
 	/*
 	 * obtain the physical base address of the PCIe controller's APB 
 	 * registers and map it to a virtual address accessible by the 
