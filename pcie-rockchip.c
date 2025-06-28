372490db08ace2d84a85bd927b5a1eaa0e7f391e
Lizhe <sensor1010@163.com>
2025-06-28 06:48:55 -0700

    host: add comment for parse_dt
    Signed-off-by: Lizhe <sensor1010@163.com>

--- pcie-rockchip.c
+++ pcie-rockchip.c
@@ -1,7 +1,11 @@
 
 int rockchip_pcie_parse_dt(struct rockchip_pcie *rockchip)
 {
-	
+	/*
+	 * obtain the physical base address of the PCIe controller's APB 
+	 * registers and map it to a virtual address accessible by the 
+	 * kernel
+	 */	
 	rockchip->apb_base = 
 		devm_platform_ioremap_resource_byname(pdev, "apb-base");
 	if (IS_ERR(rockchip->apb_base))
