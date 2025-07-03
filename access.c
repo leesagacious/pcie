af69079929f2f7265d53cfba687d569187e1b44c
Lizhe <sensor1010@163.com>
2025-07-03 09:31:37 -0700

    rw: add function pci_read_config_byte
    Signed-off-by: Lizhe <sensor1010@163.com>

---
+++ access.c
@@ -0,0 +1,10 @@
+
+int pci_read_config_byte(const struct pci_dev *dev, int where, u8 *val)
+{
+	if (pci_dev_is_disconnected(dev)) {
+		*val = ~0;
+		return PCIBIOS_DEVICE_NOT_FOUND;
+	}
+
+	return pci_bus_read_config_byte(dev->bus, dev->devfn, where, val);
+}
