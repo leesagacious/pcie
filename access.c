88e0166fd4b2eab6fd7b6221aceddd65161c763d
Lizhe <sensor1010@163.com>
2025-07-03 09:33:58 -0700

    rw: add comment of pci_read_config_byte
    Signed-off-by: Lizhe <sensor1010@163.com>

--- access.c
+++ access.c
@@ -1,4 +1,15 @@
-
+/*
+ * read a byte from the configuration space of the PCIe device.
+ *
+ * the PCIe specification explicitly states that only the RC has
+ * the permission to originate configuration requests. 
+ * this means that other devices within the PCIe system are not 
+ * allowed to access or configuration the configuration space of 
+ * other devices
+ *
+ * Additionally, the routing method for configuration requests must
+ * be based on BDF
+ */
 int pci_read_config_byte(const struct pci_dev *dev, int where, u8 *val)
 {
 	if (pci_dev_is_disconnected(dev)) {
