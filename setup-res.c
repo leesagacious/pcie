66d42bfaa3d78b71cfb860dc40ee2b29f8b741dc
Lizhe <sensor1010@163.com>
2025-07-12 09:19:20 -0700

    resource: add comment for COMMAND register
    Signed-off-by: Lizhe <sensor1010@163.com>

--- setup-res.c
+++ setup-res.c
@@ -7,6 +7,11 @@ int pci_enable_resources(struct pci_dev *dev, int mask)
 	/*
 	 * read the command register from the PCIe configuration
 	 * space
+	 *
+	 * command:
+	 * 	b0	I/O Space Enable启用设备的 I/O 空间访问
+	 * 	b1	Memory Space Enable启用设备的内存空间访问
+	 * 	b2	Bus Master Enable允许设备作为总线主控发起 DMA 操作
 	 */
 	pci_read_config_word(dev, PCI_COMMAND, &cmd);
 	old_cmd = cmd;
